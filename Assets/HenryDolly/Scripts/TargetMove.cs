1da499f284443099f6d191aa4f1a24e22af1956b
Henry Shin <taeuk.shin1@gmail.com>
2021-07-11 01:12:23 +0900

    First Commit

---
+++ Assets/HenryDolly/Scripts/TargetMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMove : MonoBehaviour
+{
+    public float speed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        Vector3 dir = v * Vector3.forward + h * Vector3.right;
+
+        print(dir);
+        transform.position += dir * speed * Time.deltaTime;
+        
+    }
+
+    public void MoveToPosition(Transform pos){
+        transform.position = pos.position;
+    }
+}
